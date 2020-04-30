b8d305969339e3d521c0b30c02deb11e03900363
keenmukund <mukundzplace@gmail.com>
2020-04-30 22:36:45 +0530

    added module 3

---
+++ Module 3/StudentID.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class StudentID 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String fname;
+        String lname;
+        String date;
+        System.out.println("Enter first name: ");
+        fname=sc.nextLine();
+        System.out.println("Enter last name: ");
+        lname=sc.nextLine();
+        System.out.println("Enter Date of Birth (dd/mm/yyyy): ");
+        date=sc.nextLine();
+        System.out.println("Student ID is: ");
+        System.out.println(" " + fname.charAt(0) + lname.charAt(0) + date.charAt(0) + date.charAt(1));
+    } 
+}
